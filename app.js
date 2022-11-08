f8975a84e1d3f2a5c4817373c9a5cbd7a0abe853
NaoSan9 <naomi.santos@outlook.fr>
2022-11-08 13:53:44 +0100

    Validators for Movie & User added on POST and PUT routes

--- app.js
+++ app.js
@@ -15,16 +15,19 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
+const { validateMovie, validateUser } = require("./validators.js");
+
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies", validateMovie, movieHandlers.updateMovie);
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
-app.put("/api/users/:id", userHandlers.updateUser);
+app.post("/api/users", validateUser, userHandlers.postUser);
+app.put("/api/users/:id", validateUser, userHandlers.updateUser);
+
 
 app.listen(port, (err) => {
   if (err) {
