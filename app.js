140a7f51468f38ab7d553fb9a8a0b1953cdf9640
NaoSan9 <naomi.santos@outlook.fr>
2022-12-12 16:53:34 +0100

    Hashing password in the database implemented

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.get("/", welcome);
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
 const { validateMovie, validateUser } = require("./validators.js");
+const { hashPassword } = require("./auth.js");
 
 
 app.get("/api/movies", movieHandlers.getMovies);
@@ -26,6 +27,7 @@ app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", hashPassword, userHandlers.postUser);
 app.post("/api/users", validateUser, userHandlers.postUser);
 app.put("/api/users/:id", validateUser, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
--- userHandlers.js
+++ userHandlers.js
@@ -56,12 +56,12 @@ const getUserById = (req, res) => {
 };
 
 const postUser = (req, res) => {
-    const { firstname, lastname, email, city, language } = req.body;
+    const { firstname, lastname, email, city, language, hashedPassword } = req.body;
 
     database
         .query(
-            "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-            [firstname, lastname, email, city, language]
+            "INSERT INTO users(firstname, lastname, email, city, language, hashedPassword) VALUES (?, ?, ?, ?, ?, ?)",
+            [firstname, lastname, email, city, language, hashedPassword]
         )
         .then(([result]) => {
             res.location(`/api/users/${result.insertId}`).sendStatus(201);
@@ -79,7 +79,7 @@ const updateUser = (req, res) => {
 
     database
         .query(
-            "update movies set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+            "update movies set firstname = ?, lastname = ?, email = ?, city = ?, language = ?, where id = ?",
             [firstname, lastname, email, city, language, id]
         )
         .then(([result]) => {
