5146015be98ad0682a68c2a334d1c09d221ac294
NaoSan9 <naomi.santos@outlook.fr>
2022-12-19 11:08:20 +0100

    Public and protected routes functional, user permission to delete or update functional.

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
+
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
@@ -15,28 +23,27 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
-const { validateMovie, validateUser } = require("./validators.js");
-const { hashPassword } = require("./auth.js");
+const { validateMovie, validateUser } = require("./validators");
+const { hashPassword, verifyPassword, verifyToken, userPermissionsById } = require("./auth");
 
+// Public routes //
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-app.put("/api/movies", validateMovie, movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", hashPassword, userHandlers.postUser);
+app.post("/api/login", userHandlers.getUserByEmailWithPasswordAndPassToNext, verifyPassword);
+
+// Protected routes //
+
+app.use(verifyToken);
+
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies", validateMovie, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
 app.post("/api/users", validateUser, userHandlers.postUser);
-app.put("/api/users/:id", validateUser, userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
-
-
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
-});
+app.put("/api/users/:id", validateUser, userPermissionsById, userHandlers.updateUser);
+app.delete("/api/users/:id", userPermissionsById, userHandlers.deleteUser);
